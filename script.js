54e6038a721fe01245ca1b93878abf3875069b1a
Phil <pamramfam@gmail.com>
2023-11-06 20:36:16 -0500

    built out more data in JS file

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const EVENTS_ENDPOINT= `${BASE_URL}/events`;
 
 
 const PARTY_LIST = document.getElementById('partyList'); 
+const formElement = document.getElementById('partyForm'); 
+const partyName = document.getElementById('partyName'); 
+const partyDate = document.getElementById('partyDate'); 
+const partyLocation = document.getElementById('partyLocation'); 
+const partyDescription = document.getElementById('partyDescription'); 
+const partyTime = document.getElementById('partyTime'); 
+
+
+
+formElement.addEventListener('submit', function (event) { 
+    event.preventDefault(); 
+    const newParty = { name: partyName.value, description: partyDescription.value, date: `${partyDate.value}T${partyTime.value}:00Z`, location: partyLocation.value }
+    console.log(newParty); 
+    createEvent(newParty); 
+})
+
 
 async function fetchEvents() {
     try {
@@ -16,15 +32,12 @@ async function fetchEvents() {
         const events = jsonResponse.data; 
         renderEvents(events); 
 
-
     } catch (error) {
         console.error(error); 
     }
 }
 
 fetchEvents(); 
-
-
 
 
 function renderEvents(events) { 
@@ -34,5 +47,22 @@ function renderEvents(events) {
         PARTY_LIST.append(eventListItem); 
     }
 } 
+
+async function createEvent(event) {
+    try {
+        const response = await fetch( EVENTS_ENDPOINT, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(event) } ); 
+        if (!response.ok) {
+            console.log("API error", response); 
+            return; 
+        }
+        const jsonResponse = await response.json(); 
+        const events = jsonResponse.data; 
+        // renderEvents(events); 
+        console.log(events); 
+
+    } catch (error) {
+        console.error(error); 
+    }
+}
 
 
