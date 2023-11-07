cd68a70503cde7bbb6a4dfb456593af09d4b28ec
Phil <pamramfam@gmail.com>
2023-11-06 19:28:47 -0500

    rendering live events

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 // // Define base URL and cohort constant 
-const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api'; 
-const COHORT = '2308-ACC-ET-WSEB-PT-B'; 
+const BASE_URL = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-ET-WSEB-PT-B'; 
+const EVENTS_ENDPOINT= `${BASE_URL}/events`; 
+
 
 const PARTY_LIST = document.getElementById('partyList'); 
 
-const events = [ 
-    { 
-        id: 1100, 
-        name: 'Steve Party',
-        description: 'Lorem ipusm', 
-        date: "2023-12-20T10:00:00",
-        location: "20392 Cherry Street",
-        cohort: 13
-    }, 
-    { 
-        id: 1200, 
-        name: 'John Party',
-        description: 'Lorem ipusm', 
-        date: "2023-12-20T10:00:00",
-        location: "20392 Cherry Street",
-        cohort: 14
-    }, 
-    {
-        id: 1210, 
-        name: 'Griffin Party',
-        description: 'Lorem ipusm', 
-        date: "2023-12-20T10:00:00",
-        location: "20392 Cherry Street",
-        cohort: 15
-    }, 
-]; 
+async function fetchEvents() {
+    try {
+        const response = await fetch(EVENTS_ENDPOINT); 
+        if (!response.ok) {
+            console.log("API error", response.status); 
+            return; 
+        }
+        const jsonResponse = await response.json(); 
+        const events = jsonResponse.data; 
+        renderEvents(events); 
 
 
-function renderEvents() { 
+    } catch (error) {
+        console.error(error); 
+    }
+}
+
+fetchEvents(); 
+
+
+
+
+function renderEvents(events) { 
     for (let event of events) { 
         let eventListItem = document.createElement("li"); 
         eventListItem.textContent = `Name: ${event.name} Description ${event.description} Date: ${event.date} Location: ${event.location}`; 
@@ -40,4 +35,4 @@ function renderEvents() {
     }
 } 
 
-renderEvents(); 
+
